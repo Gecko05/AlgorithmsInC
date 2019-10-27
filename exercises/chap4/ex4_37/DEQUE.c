87bff409ed840440a06a99ce85f14f95348a1238
Gecko05 <jacacent@gmail.com>
2019-10-26 19:41:16 -0500

    New ex

---
+++ exercises/chap4/ex4_37/DEQUE.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include "Item.h"
+
+typedef struct QUEUEnode *link;
+struct QUEUEnode{
+    Item item;
+    link next;
+    link prev;
+};
+
+static link head;
+static link tail;
+
+link NEW(Item item, link next)
+{
+    link x = malloc(sizeof *x);
+    x->item = item;
+    x->next = next;
+    return x;
+}
+
+void QUEUEerror(){
+    printf("Operation not available\n");
+}
+
+void QUEUEinit(int maxN){
+    head = NULL;
+}
+
+int QUEUEempty(){
+    return head == NULL;
+}
+// Place at the end of the
+void QUEUEput(Item item)
+{   if (head == NULL){
+        tail = NEW(item,head);
+        head = tail;
+        return;
+    }
+    else{
+        QUEUEerror();
+        return;
+    }
+    tail->next = NEW(item, tail->next);
+    tail = tail->next;
+}
+
+Item QUEUEget()
+{
+    if (QUEUEempty()){
+        QUEUEerror();
+        return;
+    }
+    Item item = head->item;
+    link t = head->next;
+    free(head);
+    head = t;
+    return item;
+}
