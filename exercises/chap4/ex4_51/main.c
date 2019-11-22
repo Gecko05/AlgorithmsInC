3c48646e261b3eafdd3be2aef02d37a981353e55
Gecko05 <jacacent@gmail.com>
2019-11-21 19:55:52 -0600

    ex4_51

---
+++ exercises/chap4/ex4_51/main.c
@@ -0,0 +1,31 @@
+#include "STACK.h"
+#include "Item.h"
+#include <assert.h>
+#include <stdio.h>
+
+void dumpStack(){
+	printf("Dumping stack... ");
+	while (STACKempty() == 0){
+		Item a = STACKpop();
+		printf(" %i", a);
+	}
+	printf("\n");
+}
+
+int main(int argc, char *argv[]){
+	STACKinit(10);
+	Item a = 2;
+	Item b = 4;
+	STACKpush(a);
+	STACKpush(b);
+	STACKpush(a);
+	printf("Pushed Items...\n");
+	assert(STACKpop() == a);
+	assert(STACKpop() == b);
+
+	printf("Popped Items...\n");
+	assert(STACKpop() == -1);
+	assert(STACKempty() == 1);
+	
+	printf("Successful execution\n");
+}
