b63a0915770a57327ce1a95c1cdeff7a8946d788
Gecko05 <jacacent@gmail.com>
2020-03-21 14:17:43 -0600

    ex5_65 in progress

---
+++ exercises/chap5/ex5_65/main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node* link;
+struct node{
+    int item;
+    link r;
+    link l;
+    link p;
+};
+
+int areIsomoprhic(link a, link b)
+{
+    link x, y;
+    x = a;
+    y = a;
+    while (x != NULL){
+        
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+}
