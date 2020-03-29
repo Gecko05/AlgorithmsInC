c2514f99e68b0ebd3a0e4200655b3eeb51be1c4d
Gecko05 <jacacent@gmail.com>
2020-03-29 11:01:34 -0600

    ex5_88

---
+++ exercises/chap5/ex5_88/main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "TREE.h"
+
+#define N 10
+
+// Function to compute the internal path length
+
+int countNodes(link x, int l)
+{
+    if (x == NULL){
+        // Node doesn't exist
+        return 0;
+    }
+    if (x->l == NULL && x->r == NULL){
+        // Node is external, no children
+        return 0;
+    }
+    // Node is internal because it has children
+    int u = countNodes(x->l, l+1);
+    int v = countNodes(x->r, l+1);
+
+    return l + u + v;
+}
+
+int main(int argc, char *argv[]){
+    link root = newTree(N);
+    int level = 0;
+    int pathLen = countNodes(root, level);
+    printf("Internal path Len: %i\n", pathLen);
+}
