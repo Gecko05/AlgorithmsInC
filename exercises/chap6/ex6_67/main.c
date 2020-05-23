7e861a0ec874dd78f8b86b6cfeb2c656b0b3638d
Gecko05 <jacacent@gmail.com>
2020-05-23 13:20:10 -0500

    ex6_67

--- exercises/chap6/ex6_67/main.c
+++ exercises/chap6/ex6_67/main.c
@@ -21,9 +21,12 @@ link insertion_list(link h)
     link a = h;
     link t;
     link x;
+    link u;
     b->next = NULL;
 
-    for (t = a->next; t != NULL; t = t->next){
+    for (t = a->next; t != NULL; t = u){
+        // Still figuring out why this works instead of t = t->next
+        u = t->next;
         for (x = b; x->next != NULL; x = x->next){
             if (x->next->item > t->item){
                 break;
@@ -40,25 +43,25 @@ int main(){
     int N = 8;
     int i;
     link head_a = malloc(sizeof(*head_a));
-    
+
     for (i = 0, t = head_a; i < N; i++){
         t->next = malloc(sizeof(*t));
         t = t->next;
         t->next = NULL;
         t->item = rand() % 1000;
     }
-}
-b = &headb;
-b->next = NULL;
-for (t = a->next; t != NULL; t = u)
-{
-    u = t->next;
-    for (x = b; x->next != NULL; x = x->next)
-    {
-        if (x->next->item > t->item){
-            break;
-        }
+
+    link x = head_a;
+    while(x != NULL){
+        printf("%i ", x->item);
+        x = x->next;
     }
-    t->next = x->next;
-    x->next = t;
+    printf("\n");
+    head_a = insertion_list(head_a);
+    x = head_a;
+    while(x != NULL){
+        printf("%i ", x->item);
+        x = x->next;
+    }
+    printf("\n");
 }
