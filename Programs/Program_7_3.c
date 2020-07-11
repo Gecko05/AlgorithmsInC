35f1b3c88f6d427ed84c35580cdd29b3941c9788
Gecko05 <jacacent@gmail.com>
2020-07-11 12:50:36 -0500

    ex7_14

---
+++ Programs/Program_7_3.c
@@ -0,0 +1,25 @@
+// Program 7.3 Nonrecursive quicksort
+
+#define push2(A, B) push(B); push(A);
+
+void quicksort(Item a[], int l, int r)
+{
+    int i;
+    stackinit();
+    push2(l, r);
+    while (!stackempty())
+    {
+        l = pop();
+        r = pop();
+        if (r <= l) continue; // Nothing to do
+        i = partition(a, l, r);
+        if (i-l > r-i){ // Find the biggest partition
+            push2(l, i-1);
+            push2(i+1, r);
+        }
+        else{
+            push2(i+1, r);
+            push2(l, i-1);
+        }
+    }
+}
--- exercises/chap7/bonusQuicksort/main.c
+++ exercises/chap7/bonusQuicksort/main.c
@@ -2,12 +2,21 @@
 #include "Item.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include <time.h>
 
-#define exch(a, b) 
+#define exch(a, b) Item c = a; a = b; b = c;
 
 int less(Item a, Item b)
 {
     return a < b;
+}
+
+void show(Item *a, int n)
+{
+    for (int i = 0; i < n; i++){
+        printf("%i ", a[i]);
+    }
+    printf("\n");
 }
 
 // Partioning for quicksort
@@ -35,6 +44,7 @@ int partition(Item a[], int l, int r)
 }
 
 void quicksort(Item a[], int l, int r){
+    static int c = 0;
     int i;
     if (r <= l){
         return;
@@ -42,14 +52,22 @@ void quicksort(Item a[], int l, int r){
     i = partition(a, l, r);
     quicksort(a, l, i-1);
     quicksort(a, i+1, r);
+    c++;
+    printf("Calls %i\n", c);
 }
 
 int main(int argc, char *argv[])
 {
+    time_t t;
+    srand((unsigned) time(&t));
     int N = 10;
     Item *s = malloc(sizeof(*s) * N);
     for (int i = 0; i < N; i++){
+        //s[i] = rand() % 10;
         s[i] = i;
     }
+    show(s, N);
+    quicksort(s, 0, N);
+    show(s, N);
     free(s);
 }
