8e221baaa14b6f5bcae7bafa2cea2108ee825435
Gecko05 <jacacent@gmail.com>
2020-07-11 13:15:37 -0500

    bonus quicksort with merged programs

--- exercises/chap7/bonusQuicksort/main.c
+++ exercises/chap7/bonusQuicksort/main.c
@@ -1,8 +1,9 @@
-// Quicksort Program 7.1 and 7.2 merged WIP
+// Quicksort Program 7.1 and 7.2 merged, and added non-recursive quicksort
 #include "Item.h"
 #include "stdlib.h"
 #include "stdio.h"
 #include <time.h>
+#include "DEQUE.h"
 
 #define exch(a, b) Item c = a; a = b; b = c;
 
@@ -56,6 +57,31 @@ void quicksort(Item a[], int l, int r){
     printf("Calls %i\n", c);
 }
 
+// ------------------ Non recursive Quicksort --------------- //
+#define push2(A, B) if (B > A){ QUEUEpush(B); QUEUEpush(A);}
+
+void quicksort2(Item a[], int l, int r)
+{
+    int i;
+    QUEUEinit();
+    push2(l, r);
+    while (!QUEUEempty())
+    {
+        l = QUEUEpop();
+        r = QUEUEpop();
+        //if (r <= l) continue; // Nothing to do
+        i = partition(a, l, r);
+        if (i-l > r-i){ // Find the biggest partition
+            push2(l, i-1);
+            push2(i+1, r);
+        }
+        else{
+            push2(i+1, r);
+            push2(l, i-1);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     time_t t;
@@ -63,11 +89,11 @@ int main(int argc, char *argv[])
     int N = 10;
     Item *s = malloc(sizeof(*s) * N);
     for (int i = 0; i < N; i++){
-        //s[i] = rand() % 10;
-        s[i] = i;
+        s[i] = rand() % 10;   // Random file
+        //s[i] = i;             // Degenerate file
     }
     show(s, N);
-    quicksort(s, 0, N);
+    quicksort2(s, 0, N);
     show(s, N);
     free(s);
 }
